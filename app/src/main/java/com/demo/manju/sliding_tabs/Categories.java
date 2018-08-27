610b19c4df24d91ea9ad5bde554b7879b3ec28ea
Manjunath-Akalawadi <manjuakalawadi@gmail.com>
2018-08-28 01:26:45 +0530

    Initial commit

---
+++ app/src/main/java/com/demo/manju/sliding_tabs/Categories.java
@@ -0,0 +1,18 @@
+package com.demo.manju.sliding_tabs;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Categories extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.categories,container,false);
+    }
+}
---
+++ app/src/main/java/com/demo/manju/sliding_tabs/Home.java
@@ -0,0 +1,19 @@
+package com.demo.manju.sliding_tabs;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home,container,false);
+    }
+
+}
